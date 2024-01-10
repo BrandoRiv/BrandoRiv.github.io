008e2f2987f80934c77e629e1c445ab79a88c9c8
brandoriv <brandoriv.dev@gmail.com>
2024-01-09 19:49:46 -0500

    initial commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Intro() {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1>Brandon Rivera</h1>
+            <p>Aspiring full sgineer and Web Developer</p>
+            <p>sample text</p>
+        </div>
+    )
+}
+
+export default Intro;
