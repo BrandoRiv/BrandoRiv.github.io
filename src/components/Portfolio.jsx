a571e07607c7200ef1fcffe047fff0ef1ed64a15
Brandon Rivera <brandoriv.dev@gmail.com>
2024-09-19 18:28:11 -0400

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ function Portfolio() {
   return (
     <div className="flex flex-col h-screen">
       <h1 className="text-3xl font-bold text-center my-8">Projects</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-8 py-9 h-full bg-slate-500">
+      <div className="flex flex-col gap-4 px-8 py-9 h-full bg-slate-500">
         {portfolioData.map((item) => (
           <PortfolioItem
             key={item.id}
@@ -17,6 +17,8 @@ function Portfolio() {
             description={item.description}
             link={item.link}
             github={item.github}
+            startDate={item.startDate}
+            endDate={item.endDate}
           />
         ))}
       </div>
