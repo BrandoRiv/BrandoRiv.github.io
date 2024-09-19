7a572306d0591f99f3c7117dea4dec55a243bc1b
Brandon Rivera <brandoriv.dev@gmail.com>
2024-09-19 11:15:08 -0400

    Styling update

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,38 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="bg-gradient-to-b from-[#FBFAF5] to-[#f0ebe6]">
-			<Navbar />
+	  <div className="bg-gradient-to-b from-[#FBFAF5] to-[#f0ebe6] min-h-screen">
+		<Navbar />
+		
+		{/* Main layout with sidebars */}
+		<div className="flex">
+		  {/* Left Sidebar (empty) */}
+		  <div className="hidden md:block w-1/6"></div>
+  
+		  {/* Main Content */}
+		  <div className="flex-grow p-4">
 			<div id="intro">
-				<Intro />
+			  <Intro />
 			</div>
 			<div id="about">
-				<About />
+			  <About />
 			</div>
 			<div id="portfolio">
-				<Portfolio />
+			  <Portfolio />
 			</div>
 			<div id="contact">
-				<Contact />
+			  <Contact />
 			</div>
-			<Footer />
+		  </div>
+  
+		  {/* Right Sidebar (empty) */}
+		  <div className="hidden md:block w-1/6"></div>
 		</div>
+  
+		<Footer />
+	  </div>
 	);
-}
+  }
 
 export default App;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,22 +4,10 @@ import lottie from "lottie-web";
 function Intro() {
 	const animationContainer = useRef(null);
 
-	useEffect(() => {
-		const anim = lottie.loadAnimation({
-			container: animationContainer.current,
-			renderer: "svg",
-			loop: true,
-			autoplay: true,
-			path: "src/assets/IntroGradient.json",
-		});
-
-		return () => anim.destroy();
-	}, []);
-
 	return (
 		<div className="flex justify-center items-center min-h-screen h-screen">
 			<div>
-				<div className="text-white text-6xl lg:text-8xl font-bold text-center tracking-wider">
+				<div className="text-black text-6xl lg:text-8xl font-bold text-center tracking-wider">
 					BRANDON
 				</div>
 				<div className="outline-text text-6xl lg:text-8xl font-bold text-center tracking-wider">
